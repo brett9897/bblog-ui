adfb5ac8ed465cbadf2322d52820646981feae91
brett-koenig <brettkoenig@Mrs-MacBook-Pro-2.local>
2016-11-11 17:54:25 -0500

    Installed bootstrap 4

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React, { PropTypes } from 'react';
 import { IndexLink } from 'react-router';
+import 'jquery/dist/jquery.js';
+import 'bootstrap/dist/js/bootstrap.js';
+import '../styles/styles.scss';
 
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,8 +3,15 @@ import React from 'react';
 export class HomePage extends React.Component {
   render() {
     return (
-      <div>
-        <p>This is my home page!</p>
+      <div className="container">
+        <div className="row">
+          <div className="col-xs">
+            <p>This is my home page!</p>
+          </div>
+          <div className="col-xs">
+            <p>This is col 2</p>
+          </div>
+        </div>
       </div>
     );
   }

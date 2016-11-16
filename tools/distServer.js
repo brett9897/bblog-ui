533884cb9b5916dd112f449bd69ffb7b9309d250
brett-koenig <brettkoenig@Mrs-MacBook-Pro-2.local>
2016-11-15 23:45:32 -0500

    Setting max age to a day for testing

--- tools/distServer.js
+++ tools/distServer.js
@@ -11,11 +11,7 @@ console.log(chalkProcessing('Starting production build...'));
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static('dist'));
-
-app.get('robots.txt', (req, res) => {
-  res.sendfile(path.join(__dirname, '../dist/robots.txt'));
-});
+app.use(express.static('dist', {maxAge: '1d'}));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
